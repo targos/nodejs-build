57702b50ce230810afae01301a343880354c5412
Michaël Zasso <targos@protonmail.com>
2025-02-09 17:06:03 +0100

    wip: use GIT_REMOTE_REF and add test stage

--- jenkins/pipelines/nodejs-test-linter.groovy
+++ jenkins/pipelines/nodejs-test-linter.groovy
@@ -7,12 +7,18 @@ pipeline {
         stage("Setup repository") {
             steps {
                 checkout changelog: false, poll: false, scm: scmGit(
-                    branches: [[name: 'refs/heads/_jenkins_local_branch']],
+                    branches: [[name: "$params.GIT_REMOTE_REF"]],
                     userRemoteConfigs: [[
                         credentialsId: 'github-app-node-js-ci',
                         url: "https://github.com/${params.GITHUB_ORG}/${params.REPO_NAME}.git"
                     ]]
                 )
+            }
+        }
+
+        stage("Test") {
+            steps {
+                sh "ls"
             }
         }
     }
